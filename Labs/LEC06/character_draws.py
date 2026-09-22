e3165eeafbac85f3cd0c2afa52e54fa43587b244
kchlhj0214 <wlsdlf117@naver.com>
2026-09-22 16:45:42 +0900

    LEC06: 사각 운동의 상단 이동 구현 및 위치와 방향 확인
    상단에서 오른쪽부터 왼쪽으로 이동하도록 구현하고 나머지 구간은 주석 처리함.

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -21,22 +21,31 @@ def move_circle():
         draw_boy(x, y)
 
 def move_top():
-    print('top')
+    for x in range(600, 200, -5):
+            draw_boy(x, 500)
 
-def move_right():
-    print('right')
+# def move_right():
+#     for y in range(100, 500, 5):
+#             draw_boy(600, y)
 
-def move_bottom():
-    print('bottom')
+# def move_bottom_1():
+#     for x in range(400, 600, 5):
+#         draw_boy(x, 100)
 
-def move_left():
-    print('left')
+# def move_bottom_2():
+#     for x in range(200, 400, 5):
+#         draw_boy(x, 100)
 
-def move_rectangle():
+# def move_left():
+#     for y in range(500, 100, -5):
+#             draw_boy(200, y)
+
+def move_rectangle():     
+    # move_bottom_1()
+    # move_right()
     move_top()
-    move_right()
-    move_bottom()
-    move_left()
+    # move_left()
+    # move_bottom_2()
 
 def move_triangle():
     print('triangle')
