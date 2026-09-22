276d9960bbc18c627f612ac59d07c9e64ed5e77b
kchlhj0214 <wlsdlf117@naver.com>
2026-09-22 15:57:01 +0900

    LEC06: 도형별 이동 함수 틀과 반복 호출 추가

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,15 @@
 # 실습 과제 진행
 
-while True:
+def move_circle():
     pass
+
+def move_rectangle():
+    pass
+
+def move_triangle():
+    pass
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
